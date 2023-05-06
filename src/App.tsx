324d004a19cace4d39e83b349f86071577f245b5
Zyad <42823027+zyadcodes@users.noreply.github.com>
2023-05-06 12:50:03 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from "./logo.svg";
 import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles.done}>Hiiii</div>;
+  return (
+    <div className={styles.done}>
+      Hiiii
+    </div>
+  );
 }
 
 export default App;
