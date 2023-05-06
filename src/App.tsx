ffec11c37ce2bd013b24f845ccf65273d9672fde
Zyad <42823027+zyadcodes@users.noreply.github.com>
2023-05-06 12:34:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import logo from "./logo.svg";
-import styles from "./App.module.css";
+import styles from "./App.module.scss";
 
 function App() {
-  return <div className={styles.green}>Hiiii</div>;
+  return <div className={styles.done}>Hiiii</div>;
 }
 
 export default App;
