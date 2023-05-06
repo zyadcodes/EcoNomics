1efa558cc0bf31fd36fbad1d39889e07bc4065d7
omar-abdel-aziz <99376334+omar-abdel-aziz@users.noreply.github.com>
2023-05-06 15:08:59 -0700

    data

---
+++ src/Utils/Data.ts
@@ -0,0 +1,5 @@
+export const Data = [
+  { name: "Electricity", spent: 150, waste: 400, avg: 450 },
+  { name: "Gas", spent: 350, waste: 2200, avg: 2140 },
+  { name: "Shopping", spent: 500, waste: 5000, avg: 4109 },
+];
