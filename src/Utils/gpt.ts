5074acc55c619f16c971651e46619e526ab5cc8b
omar-abdel-aziz <99376334+omar-abdel-aziz@users.noreply.github.com>
2023-05-06 12:24:12 -0700

    added folder structure

---
+++ src/Utils/gpt.ts
@@ -0,0 +1,29 @@
+const fetchCompletion = async () => {
+  const OPENAI_API_KEY = "your_openai_api_key_here";
+  const apiUrl = "https://api.openai.com/v1/chat/completions";
+
+  const headers = new Headers();
+  headers.append("Content-Type", "application/json");
+  headers.append("Authorization", `Bearer ${OPENAI_API_KEY}`);
+
+  const requestBody = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: "Hello!" }],
+  };
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(requestBody),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.error("Error fetching completion:", error);
+  }
+};
+
+export default fetchCompletion;
