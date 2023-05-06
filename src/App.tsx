979f4e35b7a1ca84be3b87d9db2af67912c13019
omar-abdel-aziz <99376334+omar-abdel-aziz@users.noreply.github.com>
2023-05-06 13:59:59 -0700

    card

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import styles from "./App.module.scss";
 import { callChatGPT } from "./Utils/gpt";
+import Card from "./Components/Card";
 
 function App() {
-  return <div className={styles.done}>Hiiii</div>;
+  return (
+    <div className={styles.done}>
+      Hiiii
+      <Card
+        iconName="electricity"
+        name="Electricity"
+        cost={5}
+        footPrint={30}
+        avg={50}
+      />
+    </div>
+  );
 }
 
 export default App;
